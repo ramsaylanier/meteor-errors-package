19dd73dda680f405190cea5ee59328aa9eb0df06
Ramsay Lanier <ramsay@nclud.com>
2015-02-04 00:00:32 -0500

    updated package.js with git info
    Signed-off-by: Ramsay Lanier <ramsay@nclud.com>

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'ramsay:errors',
-  version: '1.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Basic error notification package - a slightly more advanced version of the Disvover Meteor tutorial',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ramsaylanier/meteor-errors-package',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
